d39989a20e949a76f59c233e800376067a52c2e8
biniyamz <binizda@gmail.com>
2015-10-28 20:15:04 +0100

    test commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+ # this is just the new modification
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
